98f18e53dacf85ffd602173c6ff6a22e4bdabde3
Dias <Deborah.Dias@br.experian.com>
2025-04-22 19:56:02 -0300

    stubbing responses

--- cypress/integration/sessions.spec.js
+++ cypress/integration/sessions.spec.js
@@ -1,5 +1,80 @@
 /// <reference types="cypress" />
 
+const thursdaySessionsData = {
+  data: {
+    intro: [
+      {
+        id: "78170",
+        title: "Cypress 9 Fundamentals",
+        startsAt: "8:30",
+        day: "Thursday",
+        room: "Jupiter",
+        level: "Introductory and overview",
+        speakers: [
+          {
+            id: "37313769-11ae-4245-93b3-e6e60d5d187c",
+            name: "Adhithi Ravichandran",
+            __typename: "Speaker",
+          },
+        ],
+        __typename: "Session",
+      },
+      {
+        id: "12345",
+        title: "GraphQL Fundamentals",
+        startsAt: "9:30",
+        day: "Thursday",
+        room: "Jupiter",
+        level: "Introductory and overview",
+        speakers: [
+          {
+            id: "37313769-11ae-4245-93b3-e6e60d5d187c",
+            name: "Adhithi Ravichandran",
+            __typename: "Speaker",
+          },
+        ],
+        __typename: "Session",
+      },
+    ],
+    intermediate: [
+      {
+        id: "85324",
+        title: " Bamboo Spec",
+        startsAt: "8:30",
+        day: "Thursday",
+        room: "Io",
+        level: "Intermediate",
+        speakers: [
+          {
+            id: "e9c40ccc-1ffd-44f5-90c2-9d69ada76073",
+            name: "Benjamin Cox",
+            __typename: "Speaker",
+          },
+        ],
+        __typename: "Session",
+      },
+    ],
+    advanced: [
+      {
+        id: "84969",
+        title: "Microservices -- The Hard Way is the right Way",
+        startsAt: "9:45",
+        day: "Thursday",
+        room: "Ganymede",
+        level: "Advanced",
+        speakers: [
+          {
+            id: "60e31e1b-2d77-4f36-8e11-4d9f8b639bc8",
+            name: "Joe Lopez",
+            __typename: "Speaker",
+          },
+        ],
+        __typename: "Session",
+      },
+    ],
+  },
+};
+
 describe("Sessions page", () => {
   beforeEach(() => {
     cy.visit("/conference");
@@ -30,20 +105,26 @@ describe("Sessions page", () => {
   });
 
   it("should filter sessions and ony display Thursday sessions when Thursday button is clicked", () => {
-    cy.intercept("POST", "http://localhost:4000/graphql").as("getSessionInfo");
+    cy.intercept(
+      "POST", 
+      "http://localhost:4000/graphql", 
+      thursdaySessionsData)
+      .as("getSessionInfo");
     cy.get("@ThursdayBtn").click();
     cy.wait("@getSessionInfo");
 
+    cy.get("[data-cy=day]").should("have.length", 4);
     cy.get("[data-cy=day]").contains("Wednesday").should("not.exist");
     cy.get("[data-cy=day]").contains("Thursday").should("be.visible");
     cy.get("[data-cy=day]").contains("Friday").should("not.exist");
   });
 
   it("should filter sessions and ony display Friday sessions when Friday button is clicked", () => {
-    cy.intercept("POST", "http://localhost:4000/graphql").as("getSessionInfo");
+    cy.intercept("POST", "http://localhost:4000/graphql", {fixture: "sessions.json"}).as("getSessionInfo");
     cy.get("@FridayBtn").click();
     cy.wait("@getSessionInfo");
-
+    
+    cy.get("[data-cy=day]").should("have.length", 4);
     cy.get("[data-cy=day]").contains("Wednesday").should("not.exist");
     cy.get("[data-cy=day]").contains("Thursday").should("not.exist");
     cy.get("[data-cy=day]").contains("Friday").should("be.visible");
